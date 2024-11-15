b584c76d17284e72680ce91e3d92a27c12613adc
Abdullah <abdullah.4mohamed@gmail.com>
2024-11-15 16:00:47 +0000

    removing the node gr native autscaling. changing the serv name.

--- deploy/terraform/modules/eks/eks-module.tf
+++ deploy/terraform/modules/eks/eks-module.tf
@@ -112,28 +112,28 @@ resource "aws_eks_cluster" "eks" {
   }
 }
 
-resource "aws_eks_node_group" "eks_nodes" {
-  cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "${var.cluster_name}-node-group"
-  node_role_arn   = var.node_role_arn
-  subnet_ids      = var.private_subnet_ids
+# resource "aws_eks_node_group" "eks_nodes" {
+#   cluster_name    = aws_eks_cluster.eks.name
+#   node_group_name = "${var.cluster_name}-node-group"
+#   node_role_arn   = var.node_role_arn
+#   subnet_ids      = var.private_subnet_ids
 
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
-  }
+#   scaling_config {
+#     desired_size = 2
+#     max_size     = 3
+#     min_size     = 1
+#   }
 
-  instance_types = ["t3.medium"]
-  ami_type       = "AL2_x86_64"
+#   instance_types = ["t3.medium"]
+#   ami_type       = "AL2_x86_64"
 
-  # depends_on = [
-  #   aws_iam_role_policy_attachment.node_worker_policy,
-  #   aws_iam_role_policy_attachment.node_cni_policy,
-  #   aws_iam_role_policy_attachment.ecr_read_only_policy,
-  # ]
+#   # depends_on = [
+#   #   aws_iam_role_policy_attachment.node_worker_policy,
+#   #   aws_iam_role_policy_attachment.node_cni_policy,
+#   #   aws_iam_role_policy_attachment.ecr_read_only_policy,
+#   # ]
 
-}
+# }
 
 output "cluster_endpoint" {
   value = aws_eks_cluster.eks.endpoint
