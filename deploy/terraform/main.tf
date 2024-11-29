f2a48eb9789dac32181d8c7772baba30325aebad
Abdullah <abdullah.4mohamed@gmail.com>
2024-11-29 17:27:19 +0000

    adding the kar module to main

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -41,6 +41,21 @@ module "eks" {
 
 }
 
+module "karpenter" {
+  source       = "./modules/karpenter"
+  cluster_name = var.cluster_name
+  eks_role_arn     = module.iam.eks_role_arn  # Pass the role_arn from IAM module
+  node_role_arn    = module.iam.node_role_arn # Pass the role_arn from IAM module
+  public_subnet_ids   = module.network.public_subnet_ids  # Pass the subnet_ids from the network module
+  private_subnet_ids  = module.network.private_subnet_ids # Pass the subnet_ids from the network module
+  vpc_id              = module.network.vpc_id             # Pass the vpc_id from the network module
+  vpc_cidr      = var.vpc_cidr     # Pass the vpc_cidr_block from the network module
+  serviceName = var.serviceName
+  region = var.region
+  cluster_endpoint = module.eks.cluster_endpoint
+  cluster_oidc_issuer_url = module.eks.cluster_oidc_issuer_url
+}
+
 
 
 
