27b16ba90c6ec6fe4875955de70029b71dde6c23
Abdullah <abdullah.4mohamed@gmail.com>
2024-12-03 00:09:19 +0000

    updating te api

--- deploy/terraform/providers.tf
+++ deploy/terraform/providers.tf
@@ -18,13 +18,12 @@ provider "aws" {
   region = var.region
 }
 
-#add
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
+    api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     args = [
       "eks", "get-token", 
@@ -39,7 +38,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
     exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
+      api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       args        = [
         "eks", 
@@ -50,6 +49,4 @@ provider "helm" {
     }
   }
 }
-
-
 
