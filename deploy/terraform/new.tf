62e24bb7de20ba544b66737a0be285b673c56db0
Abdullah <abdullah.4mohamed@gmail.com>
2024-12-05 22:25:50 +0000

    setting up providers

--- deploy/terraform/new.tf
+++ deploy/terraform/new.tf
@@ -1,12 +1,58 @@
-# Provider Configuration
 provider "aws" {
-  region              = "eu-west-2"
-  allowed_account_ids = ["626635436469"]
+    region              = var.region
+    allowed_account_ids = [var.aws_account_id]
 }
 
 provider "aws" {
-  region = "us-east-1"
-  alias  = "virginia"
+    region = "us-east-1"
+    alias  = "virginia"
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
+}
+
+provider "kubectl" {
+  apply_retry_count      = 5
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "${local.backendBucket}"
+    region = "${local.region}"
+    key    = "${local.key}"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
+  }
 }
 
 # VPC Resources
